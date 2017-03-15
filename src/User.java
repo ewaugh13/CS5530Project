257f21ac7b23ad94aa7ad845b34db972b9852909
Eric Waugh <Eric Waugh>
2017-03-15 14:40:45 -0600

    started the first part and I feel like it should be working but the sql command doesn't work with the java code even though when I copy and paste it into workbench it works there

--- src/User.java
+++ src/User.java
@@ -1,8 +1,60 @@
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class User {
-	public User()
-	{}
-
+	public User() 
+	{
+		List<String> UserLogins = new ArrayList<String>();
+	}
+	
+	public String getUser(String input, Statement stmt)
+	{
+		//String sql = get info from input and run query to get information
+		String output = "";
+		return output;
+	}
+	
+	public void insertUser(String input, Statement stmt) //for registering users
+	{
+		String[] splited = input.split(",");
+		boolean isUser;
+		if(splited[7] == "yes")
+		{
+			isUser = true;
+		}
+		else
+		{
+			isUser = false;
+		}
+		String sql = "INSERT INTO Users VALUES('" + splited[0] + "', '" + splited[1] + "', '" + splited[2] + "', " + 
+		Integer.parseInt(splited[3]) + ", '" + splited[4] + "', '" + splited[5] + "', '" + splited[6] + "', " + isUser + ")";
+		
+		ResultSet rs = null;
+	 	System.out.println("executing " + sql + "\n");
+	 	try
+	 	{
+		 	rs = stmt.executeQuery(sql);
+		 	rs.close();
+	 	}
+	 	catch(Exception e)
+	 	{
+	 		System.out.println("cannot execute the query \n");
+	 	}
+	 	finally
+	 	{
+	 		try
+	 		{
+		 		if (rs!=null && !rs.isClosed())
+		 		{
+		 			rs.close();
+		 		}
+	 		}
+	 		catch(Exception e)
+	 		{
+	 			System.out.println("cannot close result set");
+	 		}
+	 	}
+	}
 }
--- src/testdriver2.java
+++ src/testdriver2.java
@@ -1,6 +1,4 @@
 
-
-
 import java.lang.*;
 import java.sql.*;
 import java.io.*;
@@ -10,98 +8,116 @@ public class testdriver2 {
 	/**
 	 * @param args
 	 */
-	public static void displayMenu()
-	{
-		 System.out.println("        Welcome to the Uotel System     ");
-    	 System.out.println("1. search a course by cname and dname:");
-    	 System.out.println("2. enter your own query:");
-    	 System.out.println("3. exit:");
-    	 System.out.println("pleasse enter your choice:");
+	public static void displayMenu() {
+		// add more options I think based on the 14 sections but there could be
+		// more?
+		System.out.println("        Welcome to the Uotel System     ");
+		System.out.println("1. Register into Uotel System:");
+		System.out.println("2. ");
+		System.out.println("3. exit Uotel System:");
+		System.out.println("please enter your choice:");
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Example for cs5530");
-		Connector con=null;
+		Connector con = null;
 		String choice;
-        String cname;
-        String dname;
-        String sql=null;
-        int c=0;
-         try
-		 {
-			//remember to replace the password
-			 	 con= new Connector();
-	             System.out.println ("Database connection established");
-	         
-	             BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-	             
-	             while(true)
-	             {
-	            	 displayMenu();
-	            	 while ((choice = in.readLine()) == null && choice.length() == 0);
-	            	 try{
-	            		 c = Integer.parseInt(choice);
-	            	 }catch (Exception e)
-	            	 {
-	            		 
-	            		 continue;
-	            	 }
-	            	 if (c<1 | c>3)
-	            		 continue;
-	            	 if (c==1)
-	            	 {
-	            		 System.out.println("please enter a cname:");
-	            		 while ((cname = in.readLine()) == null && cname.length() == 0);
-	            		 System.out.println("please enter a dname:");
-	            		 while ((dname = in.readLine()) == null && dname.length() == 0);
-	            		 Course course=new Course();
-	            		 System.out.println(course.getCourse(cname, dname, con.stmt));
-	            	 }
-	            	 else if (c==2)
-	            	 {	 
-	            		 System.out.println("please enter your query below:");
-	            		 while ((sql = in.readLine()) == null && sql.length() == 0)
-	            			 System.out.println(sql);
-	            		 ResultSet rs=con.stmt.executeQuery(sql);
-	            		 ResultSetMetaData rsmd = rs.getMetaData();
-	            		 int numCols = rsmd.getColumnCount();
-	            		 while (rs.next())
-	            		 {
-	            			 //System.out.print("cname:");
-	            			 for (int i=1; i<=numCols;i++)
-	            				 System.out.print(rs.getString(i)+"  ");
-	            			 System.out.println("");
-	            		 }
-	            		 System.out.println(" ");
-	            		 rs.close();
-	            	 }
-	            	 else
-	            	 {   
-	            		 System.out.println("EoM");
-	            		 con.stmt.close(); 
-	        
-	            		 break;
-	            	 }
-	             }
-		 }
-         catch (Exception e)
-         {
-        	 e.printStackTrace();
-        	 System.err.println ("Either connection error or query execution error!");
-         }
-         finally
-         {
-        	 if (con != null)
-        	 {
-        		 try
-        		 {
-        			 con.closeConnection();
-        			 System.out.println ("Database connection terminated");
-        		 }
-        	 
-        		 catch (Exception e) { /* ignore close errors */ }
-        	 }	 
-         }
+		String sql = null;
+		int c = 0;
+		try {
+			// remember to replace the password
+			con = new Connector();
+			System.out.println("Database connection established");
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+			while (true) {
+				displayMenu();
+				while ((choice = in.readLine()) == null && choice.length() == 0)
+					;
+				try {
+					c = Integer.parseInt(choice);
+				} catch (Exception e) {
+
+					continue;
+				}
+				if (c < 1 | c > 3)
+					continue;
+				if (c == 1) // creating a user
+				{
+					String login;
+					String password;
+					String fullName;
+					String age; //going to need to parse this one when inserting
+					String email;
+					String phoneNumber;
+					String address;
+					String userType;
+
+					System.out.println("please enter a login name:");
+					while ((login = in.readLine()) == null && login.length() == 0)
+						;
+					System.out.println("please enter a password:");
+					while ((password = in.readLine()) == null && password.length() == 0)
+						;
+					System.out.println("please enter your full name:");
+					while ((fullName = in.readLine()) == null && fullName.length() == 0)
+						;
+					System.out.println("please enter your age:");
+					while ((age = in.readLine()) == null && age.length() == 0)
+						;
+					System.out.println("please enter your email:");
+					while ((email = in.readLine()) == null && email.length() == 0)
+						;
+					System.out.println("you can enter your phone number(optional) in the format of 303-987-1234:");
+					while ((phoneNumber = in.readLine()) == null && phoneNumber.length() == 0)
+						;
+					System.out.println("you can enter your address(optional):");
+					while ((address = in.readLine()) == null && address.length() == 0)
+						;
+					System.out.println("are you a temorary house owner(yes or no):");
+					while ((userType = in.readLine()) == null && userType.length() == 0)
+						;
+					
+
+					User users = new User();
+					String input = login + "," + password + "," + fullName + "," + age + "," + email + "," + phoneNumber + "," + address + "," + userType;
+					users.insertUser(input, con.stmt);
+				} else if (c == 2) {
+					System.out.println("please enter your query below:");
+					while ((sql = in.readLine()) == null && sql.length() == 0)
+						System.out.println(sql);
+					ResultSet rs = con.stmt.executeQuery(sql);
+					ResultSetMetaData rsmd = rs.getMetaData();
+					int numCols = rsmd.getColumnCount();
+					while (rs.next()) {
+						// System.out.print("cname:");
+						for (int i = 1; i <= numCols; i++)
+							System.out.print(rs.getString(i) + "  ");
+						System.out.println("");
+					}
+					System.out.println(" ");
+					rs.close();
+				} else {
+					System.out.println("EoM");
+					con.stmt.close();
+
+					break;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.err.println("Either connection error or query execution error!");
+		} finally {
+			if (con != null) {
+				try {
+					con.closeConnection();
+					System.out.println("Database connection terminated");
+				}
+
+				catch (Exception e) {
+					/* ignore close errors */ }
+			}
+		}
 	}
 }
