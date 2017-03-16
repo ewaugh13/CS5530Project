cbb29800a0b72118c9e7b37a49cc1169e27ae5de
Eric Waugh <Eric Waugh>
2017-03-16 13:14:34 -0600

    checking age and shit

---
+++ src/Available.java
@@ -0,0 +1,11 @@
+import java.sql.Statement;
+
+public class Available 
+{
+	public Available(){}
+	
+	public String getDatesAvailable(String THid, Statement stmt) throws Exception
+	{
+		return "";
+	}
+}
--- src/TH.java
+++ src/TH.java
@@ -1,16 +1,14 @@
-import java.util.ArrayList;
-import java.util.List;
+
+
 
 public class TH
 {
-	public TH() 
-	{
-		List<String> THIds = new ArrayList<String>(); 
-	}
+	public TH() {}
 	
-	public void insertTH()
+	public void insertTH() //check that the user login exists
 	{
-		
+		//select max THID from current ones and then add onto it by 1 and then insert values
+		//check that the user is a temporary housing owner in usertype if not update it so they are
 	}
 	
 	public void updateTH() //check for that users login name here
