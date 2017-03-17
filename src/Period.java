e8ae3f0157fbb262ac65f90dbe4ff5b1777db21a
Jacob Sivo <jake.takahashi@yahoo.com>
2017-03-16 22:21:40 -0600

    finished Implmenting Period

--- src/Period.java
+++ src/Period.java
@@ -1,47 +1,76 @@
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.regex.Pattern;
-import java.io.BufferedReader;
+import java.util.Date;
 
 
 public class Period 
 {
 	public Period(){}
 	
-	private static void insertPeriod(String input, Connection conn, Statement stmt) throws SQLException, IOException
+	private static void insertPeriod(String dateFrom, String dateTo, Connection conn, Statement stmt) throws SQLException, IOException, ParseException
 	{
-		String[] splitted = input.split(",");
-		for(int i = 0; i < splitted.length; i++)
+
+		if(Pattern.matches("\\s*", dateFrom))
 		{
-			if(Pattern.matches("\\s*", splitted[i]))
-			{
-				splitted[i] = null;
-			}
+			dateFrom = null;
+		}
+		else if(Pattern.matches("\\s*", dateTo))
+		{
+			dateTo = null;
+		}
+		String pidgetter = "select max(pid) from Period";
+		
+		
+		String output = "";
+		ResultSet rs = null;
+		rs = stmt.executeQuery(pidgetter);
+		while (rs.next())
+		{
+			output = rs.getString("max(pid)");
+		}
+		if(output == "")
+		{
+			output = "0";
 		}
 
 		String query = " insert into Period (pid, fromDate, toDate)"
 				+ " values (?, ?, ?)";
+		
+		SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd");
+		
+		try
+		{
+			Date parsedTo = format.parse(dateTo);
+			Date parsedFrom = format.parse(dateFrom);
+			
+			java.sql.Date dateFromSQL = new java.sql.Date(parsedFrom.getTime());
+			java.sql.Date dateToSQL = new java.sql.Date(parsedTo.getTime());
 
-		// create the mysql insert preparedstatement
-		PreparedStatement preparedStmt = conn.prepareStatement(query);
-		setValues(preparedStmt, splitted); //sets the values and calls execute
+			// create the mysql insert preparedstatement
+			PreparedStatement preparedStmt = conn.prepareStatement(query);
+			setValues(preparedStmt, Integer.getInteger(output), dateFromSQL, dateToSQL); //sets the values and calls execute
+		}
+		catch(Exception e)
+		{
+			System.out.println("Dates not put in the correct format please try again.");
+		}
 	}
 	
-	private static void setValues(PreparedStatement preparedStmt, String[] splitted, date dateFrom, date dateTo) throws SQLException, IOException
+	private static void setValues(PreparedStatement preparedStmt, int pid, java.sql.Date dateFrom, java.sql.Date dateTo) throws SQLException, IOException
 	{
 		try
 		{
-			preparedStmt.setInt(1, Integer.parseInt(splitted[0]));
+			preparedStmt.setInt(1, pid);
 			preparedStmt.setDate(2, dateFrom);
 			preparedStmt.setDate(3, dateTo);
 			exectueStmt(preparedStmt);
 		}
 		catch(Exception e)
 		{
-			System.out.println(e);
+			System.out.println("Incorect values please try again.");
 		}
 		
 	}
@@ -51,10 +80,11 @@ public class Period
 		try
 		{
 			preparedStmt.execute();
-			System.out.println("Successfully Favorited. \n");
+			System.out.println("Successfully created period. \n");
 		}
 		catch (SQLException e)
 		{
-			System.out.println("Can not favorite the TH. Make sure all of your values are correct. \n");
+			System.out.println("Can not create the period. \n");
 		}
+	}
 }
