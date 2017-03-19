e2e873aa8181343c6a3a364b135bf28cf2a66328
Jacob Sivo <jake.takahashi@yahoo.com>
2017-03-18 22:53:16 -0600

    prep for merge

--- src/UsefulFeedbacks.java
+++ src/UsefulFeedbacks.java
@@ -1,4 +1,21 @@
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 public class UsefulFeedbacks {
-
+	
+	public UsefulFeedbacks(){}
+	
+	public static void getUsefulFeedbacks(String THID, Connection conn, Statement stmt) throws SQLException, IOException
+	{
+		
+	}
+	
 }
