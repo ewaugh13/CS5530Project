dfd0f71623e1373937958f54e42f4d183aaf762b
Jacob Sivo <jake.takahashi@yahoo.com>
2017-03-21 21:47:43 -0600

    prepping for merge with updated Pt.11 query and useful feedbacks

--- src/UsefulFeedbacks.java
+++ src/UsefulFeedbacks.java
@@ -1,9 +1,6 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -14,9 +11,8 @@ public class UsefulFeedbacks {
 	public UsefulFeedbacks(){}
 	
 	public static void getUsefulFeedbacks(int THID, int numFeedbcks, Connection conn, Statement stmt) throws SQLException, IOException
-	{
-		String avgFeedbackScores = "select avg(score) from Feedback where THID = " + THID;
-		//String rankedFeedbacks = 
+	{				
+		
 	}
-	
+
 }
